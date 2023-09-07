2a5b2374fc58909f41834584aeab094901933c02
mohammedawadabu <mohaau2036@gmail.com>
2023-09-07 11:34:57 -0700

    o

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-int CFILE;
-export CFILE=0-main.c ; ./0-preprocessor
 
- * main - Entry point
- *
- * Description: A C program that print with put function
- *
- * Return: Alawys 0 (Success)
- 
-
+int  $CFILE;
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
