56ee197e30519cbc56f3d8361b9af92d7ab756ae
mohammedawadabu <mohaau2036@gmail.com>
2023-09-14 03:18:52 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,8 @@
  */
 void print_alphabet(void)
 {
-  int ch;
-
-  for (ch = 'a'; ch <= 'z'; ch++)
-    _putchar(ch);
-  _putchar('\n');
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
